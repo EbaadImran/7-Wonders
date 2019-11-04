78bab1cc8890429a3261d57cfbf97fc816be1d8f
Ebaad Imran <imran.ebaad@yahoo.com>
2019-11-04 13:47:42 -0600

    Update Wonder.java

--- Wonder.java
+++ Wonder.java
@@ -1,107 +1,119 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class Wonder {
-	private String[] attributes;
-	private Card[] stages;
-	private HashMap<String, Integer> usableRes;
-	private HashSet<String> tradableRes;
-	private HashMap<String, TreeSet<Card>> structures;
-	public Wonder(String att) {
-		//name|res|cost1|cost2|cost3|stage1|stage2|stage3
-		attributes = att.split("|");
-		stages = new Card[3];
-		usableRes = new HashMap<>();
-		tradableRes = new HashSet<>();
-		structures = new HashMap<>();
+public class Player 
+{
+	int turn;
+	Wonder w;
+	int coins;
+	ArrayList<Card> hand;
+	HashMap<Integer, Integer> points;
+	HashMap<String, Set<String>> oneCostRes;
+	int mPower;
+	HashMap<String, Integer> sci;
+	public Player(String att, int t)
+	{
+		turn = t;
+		w = new Wonder(att);
+		hand = new ArrayList<>();
+		coins = 3;
+		oneCostRes = new HashMap<>();
+		points = new HashMap<>();
+		mPower = 0;
+		sci = new HashMap<>();
 		
-		structures.put("brown", new TreeSet<>());
-		structures.put("silver", new TreeSet<>());
-		structures.put("blue", new TreeSet<>());
-		structures.put("gold", new TreeSet<>());
-		structures.put("red", new TreeSet<>());
-		structures.put("green", new TreeSet<>());
-		structures.put("purple", new TreeSet<>());
+		sci.put("sci1", 0);
+		sci.put("sci2", 0);
+		sci.put("sci3", 0);
 		
-		usableRes.put("wood", 0);
-		usableRes.put("stone", 0);
-		usableRes.put("ore", 0);
-		usableRes.put("clay", 0);
-		usableRes.put("loom", 0);
-		usableRes.put("glass", 0);
-		usableRes.put("papyrus", 0);
+		points.put(-1, 0);
+		points.put(1, 0);
+		points.put(3, 0);
+		points.put(5, 0);
+	}
+	public void addCoins(int i)
+	{
+		coins = coins + i;
+	}
+	public void addPoints(int name, int num)
+	{
+		//https://www.youtube.com/watch?v=b2qsRX4B_Kk pls watch LOL!!!!!!
+		int epic = points.get(name);
+		points.put(name, epic + num);
+	}
+	public int getCoins()
+	{
+		return coins;
+	}
+	public ArrayList<Card> getHand()
+	{
+		return hand;
+	}
+	public HashMap<Integer, Integer> getPoints()
+	{
+		return points;
+	}
+	public int getColorAmt(String color)
+	{
+		return w.getStructure(color).size();
+	}
+	public int getNegs()
+	{
+		return points.get(-1);
+	}
+	public int[] getScore()
+	{
+		int[] total = new int[7];
+		//military
+		total[0] += (-1*points.get(-1)) + (points.get(1)) + (3*points.get(3)) + (5*points.get(5));
+		//coins
+		total[1] += coins/3;
+		//wonder
+		Card[] cards = w.getStages();
+		total[2] += (cards[0] != null) ? 3:0;
+		total[2] += (cards[2] != null) ? 7:0;
+		//civic
+		for(Card c : w.getStructure("blue"))
+			total[3] += Integer.parseInt(c.getEffect());
+		//commercial
 		
-		usableRes.put(attributes[1], 1);
-		tradableRes.add(attributes[1]);
+		//guilds
+		
+		//science
+		total[6] += 7 * (Math.min(sci.get("sci1"), Math.min(sci.get("sci2"), sci.get("sci3"))));
+		total[6] += Math.pow(sci.get("sci1"), 2) + Math.pow(sci.get("sci2"), 2) + Math.pow(sci.get("sci3"), 2);
+		return total;
 	}
-	public void addUsable(String res)
+	public Wonder getWonder()
 	{
-		usableRes.put(res, usableRes.get(res));
+		return w;
 	}
-	public void addTradable(String res)
+	public void playCard(int joshleisajoke)
 	{
-		tradableRes.add(res);
+		//Josh Le got Iss for going to In-and-Out, he basically is the biggest monkey alive ginga gunga ginga gunga
+		w.build(hand.get(joshleisajoke));
 	}
-	public int getUsableRes(String res)
+	public boolean playable(Card oth)
 	{
-		return usableRes.get(res);
-	}
-	public Set<String> getTradableRes()
-	{
-		return tradableRes;
-	}
-	public String getCost1()
-	{
-		return attributes[2];
-	}
-	public String getCost2()
-	{
-		return attributes[3];
-	}
-	public String getCost3()
-	{
-		return attributes[4];
-	}
-	public String getStage1()
-	{
-		return attributes[5];
-	}
-	public String getStage2()
-	{
-		return attributes[6];
-	}
-	public String getStage3()
-	{
-		return attributes[7];
-	}
-	public TreeSet<Card> getStructure(String color)
-	{
-		return structures.get(color);
-	}
-	public boolean hasStructure(String oth)
-	{
-		for(String k : structures.keySet())
-			for(Card c : structures.get(k))
-				if(c.getName() == oth)
-					return true;
+		if(w.hasStructure(oth.getName()))
+			return false;
+		
+		boolean byCost = true;
+		for(String k : oth.getCost().keySet())
+			if(w.getUsableRes(k) < oth.getCost().get(k))
+				byCost = false;
+		if(byCost)
+			return true;
+		
+		if(w.hasStructure(oth.getFree()))
+			return true;
+		
 		return false;
 	}
-	public void activate(Card oth)
+	public void setHand(ArrayList<Card> eshaimran)
 	{
-		for(int i=0;i<stages.length;i++)
-		{
-			if(stages[i] == null)
-			{
-				stages[i] = oth;
-				return;
-			}
-		}
-	}
-	public void build(Card c) {
-		//ebaads mom btw LOL!
-		TreeSet<Card> yourmom = structures.get(c.getColor());
-		yourmom.add(c);
+		hand = eshaimran;
 	}
 }
