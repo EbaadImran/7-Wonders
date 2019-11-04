882bb1a01815cd724fbff013275316bdc084fa80
Ebaad Imran <imran.ebaad@yahoo.com>
2019-11-03 18:29:46 -0600

    map for costs

--- Wonder.java
+++ Wonder.java
@@ -1,37 +1,54 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Wonder {
 	private String[] attributes;
 	private Card[] stages;
-	private ArrayList<String> usableRes;
-	private ArrayList<String> tradableRes;
-	private HashMap<String, Set<Card>> structures;
+	private HashMap<String, Integer> usableRes;
+	private HashSet<String> tradableRes;
+	private HashMap<String, HashSet<Card>> structures;
 	public Wonder(String att) {
 		//name|res|cost1|cost2|cost3|stage1|stage2|stage3
 		attributes = att.split("|");
 		stages = new Card[3];
-		usableRes = new ArrayList<>();
-		tradableRes = new ArrayList<>();
+		usableRes = new HashMap<>();
+		tradableRes = new HashSet<>();
 		structures = new HashMap<>();
-		usableRes.add(attributes[1]);
+		
+		structures.put("brown", new HashSet<>());
+		structures.put("silver", new HashSet<>());
+		structures.put("blue", new HashSet<>());
+		structures.put("gold", new HashSet<>());
+		structures.put("red", new HashSet<>());
+		structures.put("green", new HashSet<>());
+		structures.put("purple", new HashSet<>());
+		
+		usableRes.put("wood", 0);
+		usableRes.put("stone", 0);
+		usableRes.put("ore", 0);
+		usableRes.put("clay", 0);
+		usableRes.put("loom", 0);
+		usableRes.put("glass", 0);
+		usableRes.put("papyrus", 0);
+		
+		usableRes.put(attributes[1], 1);
 		tradableRes.add(attributes[1]);
 	}
 	public void addUsable(String res)
 	{
-		usableRes.add(res);
+		usableRes.put(res, usableRes.get(res));
 	}
 	public void addTradable(String res)
 	{
 		tradableRes.add(res);
 	}
-	public ArrayList<String> getUsableRes()
+	public int getUsableRes(String res)
 	{
-		return usableRes;
+		return usableRes.get(res);
 	}
-	public ArrayList<String> getTradableRes()
+	public Set<String> getTradableRes()
 	{
 		return tradableRes;
 	}
@@ -59,7 +76,7 @@ public class Wonder {
 	{
 		return attributes[7];
 	}
-	public Set<Card> getStructure(String color)
+	public HashSet<Card> getStructure(String color)
 	{
 		return structures.get(color);
 	}
@@ -76,7 +93,7 @@ public class Wonder {
 	}
 	public void build(Card c) {
 		//ebaads mom btw LOL!
-		Set<Card> yourmom = structures.get(c.getColor());
+		HashSet<Card> yourmom = structures.get(c.getColor());
 		yourmom.add(c);
 	}
 }
