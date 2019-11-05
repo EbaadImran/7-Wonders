7d5cfa4e02b167b1f951f58bdec96ccc2bce7177
goodguyVincent <44009726+goodguyVincent@users.noreply.github.com>
2019-11-05 14:36:37 -0600

    Update SevenWonders.java

--- SevenWonders.java
+++ SevenWonders.java
@@ -1,4 +1,28 @@
+import java.io.IOException;
 
-public class SevenWonders {
+import javax.swing.JFrame;
 
+public class SevenWonders extends JFrame
+{
+	
+	public static void main(String[] args)
+	{
+		new SevenWonders();
+	}
+	private Board b;
+	public SevenWonders()
+	{
+		/*try {
+			b = new Board(3);
+		}
+		catch(IOException e) {
+			System.out.println("Error");
+		}*/
+		setSize(1920, 1080);
+		WondersGraphics panel = new WondersGraphics();
+		add(panel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setUndecorated(true);
+		setVisible(true);
+	}
 }
