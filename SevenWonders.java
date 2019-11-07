cfcbe35a7e79a83145a953efb5c996305a85357f
goodguyVincent <44009726+goodguyVincent@users.noreply.github.com>
2019-11-06 18:52:23 -0600

    [Intro: Gunna] Wheezy outta here Hot, hot, hot, hot Hot, hot, hot, hot Hot, hot, hot, hot Hot, hot, hot, hot  [Chorus: Gunna] Everything litty, I love when it's hot Turned up the city, I broke off the notch Got some more millis, I keep me a knot I created history and made me a lot He tried to diss me and ended on Fox We call them chopsticks 'cause they gonna chop Took her out of Follies 'cause her pussy pop I run it like Nike, we got it on lock  [Verse 1: Gunna] Cartier eye I'm the bossman in a suit but no tie I can't be sober, I gotta stay high Pour me some syrup in a Canada Dry Ridin' in the spaceship like Bonnie and Clyde Don't worry, baby, I keep me some fire Shenenehs and Birkins, she cannot decide The latest Mercedes her go-to surprise Don't sleep on miss lady, her pussy a prize Dick in her back while I'm grippin' her sides Bigger Maybach, this ain't regular size We really fly, we like pelican glide Bitch, you ain't slick, I can tell the disguise Upgraded my wrist, put baguettes in that Sky She sing, I might sign her and change her whole life I told her to gargle and work on her highs  [Chorus: Gunna] Everything litty, I love when it's hot Turned up the city, I broke off the notch Got some more millis, I keep me a knot I created history and made me a lot He tried to diss me and ended on Fox We call them chopsticks 'cause they gonna chop Took her out of Follies 'cause her pussy pop I run it like Nike, we got it on lock  [Interlude: Young Thug] Cash, money, checks, cash Addy, Birkin, brand new extendos I just wanna fuck the bitch by myself I just passed her to the dawg like my Sprite  [Verse 2: Young Thug] I took the Bentley coupe back, then I hopped in a Cayenne (Skrrt) I put the bitch in the front of the Bentley, in front of the driver (Skrrt) Ayy, man, this shit there the weed you can't smoke in the Rolls Royce, woah, woah (Yeah, yeah) I'm still double cupped up, I'm drinkin', I shoot off your tires, huh (Doo-doo-doo-doo-doo-doo-doo-doo) I'm in the coupe by myself I had to kick a door when I was 5 Keep the awards on the shelf Whole sixteen round in the fire I'm sick and tired of these young niggas act like they firin', they tellin' these lies Actin' like they the ones created this and they get all the drip from my guys Yeah, Cartier eyes Cartier coat, Cartiers the watch Cartier love, Cartier the thot Cartier specs, buffalo on the side Princess cut diamonds, they Cartier, yeah Cartier bag for the Cartier thot Sky Wrangler coupe with two hundred the dash Cartier jeans, ain't no way I can sag Ain't no way I'ma ever gon' go out bad I can't go out, no way I'ma go out I just grip on her ass and I show out I sit like a champ and I wait on a hold-out I just whip up a new Chanel Patek I whip with the wrist and I don't break the door out Turn the whole top floor to a whorehouse Hundreds racks in ones, dude brought the flood out  [Outro: Gunna] Hot, hot, hot, hot Hot, hot, hot, hot Hot, hot, hot, damn Hot, hot, hot, hot

--- SevenWonders.java
+++ SevenWonders.java
@@ -1,4 +1,5 @@
-import java.io.IOException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 
@@ -10,18 +11,25 @@ public class SevenWonders extends JFrame
 		new SevenWonders();
 	}
 	private Board b;
+	WondersGraphics panel;
 	public SevenWonders()
 	{
-		/*try {
-			b = new Board(3);
-		}
-		catch(IOException e) {
-			System.out.println("Error");
-		}*/
 		setSize(1920, 1080);
-		WondersGraphics panel = new WondersGraphics();
-		add(panel);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new WondersGraphics();
+		add(panel);
+		this.addMouseListener(new MouseAdapter()
+		{
+			public void mousePressed(MouseEvent e)
+			{
+				int x = e.getX(), y = e.getY();
+				if(panel.getIsTitleScreen()==true)
+				{
+					panel.turnOffTitleScreen();
+					repaint();
+				}
+			}
+		});
 		setUndecorated(true);
 		setVisible(true);
 	}
